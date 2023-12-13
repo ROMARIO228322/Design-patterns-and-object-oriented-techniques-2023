17653bd67eb8defe614a95d7d1569f69c6e01710
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 18:23:38 +0200

    Create RemoteController.cs

---
+++ Controlling_IoT_devices/RemoteController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlling_IoT_devices
+{
+    /// <summary>
+    /// This class is implemented as the invoker to execute the commands.
+    /// </summary>
+    internal class RemoteController
+    {
+        ICommand command;
+        public RemoteController()
+        {
+
+        }
+        public void SetCommand(ICommand command)
+        {
+            this.command = command;
+        }
+        public void ButtonPressed()
+        {
+            {
+                command.Execute();
+            }
+        }
+    }
+}
