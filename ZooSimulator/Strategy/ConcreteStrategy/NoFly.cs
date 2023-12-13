476a7927be4084598e839561c81267f5503964fb
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:54:32 +0200

    Create NoFly.cs

---
+++ ZooSimulator/Strategy/ConcreteStrategy/NoFly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooSimulator.Strategy.ConcreteStrategy
+{
+    internal class NoFly : IFlight
+    {
+        public void PerformFly()
+        {
+            Console.WriteLine("*HIDES HEAD IN SAND AS IT CANNOT FLY*");
+        }
+    }
+}
