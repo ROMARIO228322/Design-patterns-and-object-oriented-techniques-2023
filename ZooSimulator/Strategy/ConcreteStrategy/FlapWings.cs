0dbdf33d976c1c5bb31db660861fa8214a7a8d83
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:53:52 +0200

    Create FlapWings.cs

---
+++ ZooSimulator/Strategy/ConcreteStrategy/FlapWings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooSimulator.Strategy.ConcreteStrategy
+{
+    internal class FlapWings : IFlight
+    {
+        public void PerformFly()
+        {
+            Console.WriteLine("*FLIES BY FLAPS WINGS*");
+        }
+    }
+}
