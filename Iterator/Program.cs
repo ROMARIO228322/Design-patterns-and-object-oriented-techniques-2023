7eec8b37b6e132cc8e3159f8087b1782e3797550
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 18:50:34 +0200

    Create Program.cs

---
+++ Iterator/Program.cs
@@ -0,0 +1,23 @@
+﻿using Iterator.Menus;
+using System.Collections;
+using System.Drawing;
+
+namespace Iterator
+{
+    // For this exercise try complete the Iterator and Composite example introduced in the book Head First Design Patterns by O'Reilly
+    // Chapeter 9 the Iterator and Composite Patterns (starts at page 315)
+    // Link to pdf: https://github.com/ajitpal/BookBank/blob/master/%5BO%60Reilly.%20Head%20First%5D%20-%20Head%20First%20Design%20Patterns%20-%20%5BFreeman%5D.pdf
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+            DinerMenu dinerMenu = new DinerMenu();
+
+            Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+            waitress.PrintMenu();
+            
+        }
+    }
+}
