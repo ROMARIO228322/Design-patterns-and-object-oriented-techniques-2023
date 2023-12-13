86c24b14b25c03a5fa7af8ada228533ee014b2fb
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:51:34 +0200

    Create Ostrich.cs

---
+++ ZooSimulator/Contex/TypesOfBirds/Ostrich.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooSimulator.Strategy;
+
+namespace ZooSimulator.Context.TypesOfBirds
+{
+    internal class Ostrich : Bird
+    {
+        public string? Height { get; set; }
+        public string? Speed { get; set; }
+
+        //Extracting the  IFlight from the parent class using base keyword.
+        public Ostrich(IFlight flighttype) : base(flighttype)
+        {
+        }
+        public static void Run()
+        {
+            Console.WriteLine("It runs fast!");
+        }
+    }
+}
