593c7d3422bf27e71cd532f54242e8576c52d1de
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:54:57 +0200

    Create Slides.cs

---
+++ ZooSimulator/Strategy/ConcreteStrategy/Slides.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooSimulator.Strategy.ConcreteStrategy
+{
+    internal class Slides : IFlight
+    {
+        public void PerformFly()
+        {
+            Console.WriteLine("*SLIDES BY ITS BELLY*");
+        }
+    }
+}
