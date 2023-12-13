60460f96ba110c6ef0442588fda5f32485bfbfe7
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:51:54 +0200

    Create Penguin.cs

---
+++ ZooSimulator/Contex/TypesOfBirds/Penguin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooSimulator.Strategy;
+
+namespace ZooSimulator.Context.TypesOfBirds
+{
+    internal class Penguin : Bird
+    {
+        //Extracting the  IFlight from the parent class using base keyword.
+        public Penguin(IFlight flighttype) : base(flighttype)
+        {
+        }
+        public static void Walks()
+        {
+            Console.WriteLine("It walks.");
+        }
+    }
+}
