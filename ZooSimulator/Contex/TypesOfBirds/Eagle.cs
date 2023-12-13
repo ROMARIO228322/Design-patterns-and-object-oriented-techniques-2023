c67195f3e173135baa4154c4defa257b98a58ba7
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:49:18 +0200

    Create Eagle.cs

---
+++ ZooSimulator/Contex/TypesOfBirds/Eagle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooSimulator.Strategy;
+
+namespace ZooSimulator.Context.TypesOfBirds
+{
+    internal class Eagle : Bird
+    {
+        public float Speed { get; set; }
+        public float Vision_range { get; set; }
+        //Extracting the  IFlight from the parent class using base keyword.
+        public Eagle(IFlight flighttype) : base(flighttype)
+        {
+        }
+        public static void Perch()
+        {
+            Console.WriteLine("It perchs in tree!");
+        }
+        public static void SearchForPrey()
+        {
+            Console.WriteLine("It's searching for prey");
+        }
+    }
+}
