1cf51ab82e81fd299305d3c39f9daade1f2e3f8f
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 18:20:27 +0200

    Create ICommand.cs

---
+++ Controlling_IoT_devices/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlling_IoT_devices
+{
+    //This is the common interface that every receiver classes commands use.
+    internal interface ICommand
+    {
+        public void Execute();
+    }
+}
