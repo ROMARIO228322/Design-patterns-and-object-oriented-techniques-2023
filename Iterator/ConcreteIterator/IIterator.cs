c8c029ebe923bb9eec99819cc249c2879b51d0d3
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 18:44:54 +0200

    Create IIterator.cs

---
+++ Iterator/ConcreteIterator/IIterator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator.InterfaceIterator
+{
+    public interface IIterator
+    {
+        // this method returns whether or not there is next element to iterate over in MENULIST.
+        Boolean HasNext();
+        //returns the next element of the MENULIST.
+        Object Next();
+    }
+}
