2ab3e7cf8085014d3f5c3c34236c2084ebf07dd1
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 17:53:07 +0200

    Create IFlight.cs

---
+++ ZooSimulator/Strategy/IFlight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooSimulator.Strategy
+{
+    //Interface for the strategy
+    public interface IFlight
+    {
+        public void PerformFly();
+    }
+}
