b1a5cb8dcb50bff2803e9f5f962f7e4d858f0a69
ROMARIO228322 <93651044+ROMARIO228322@users.noreply.github.com>
2023-12-13 18:39:46 +0200

    Create DinerMenuIterator.cs

---
+++ Iterator/ConcreteIterator/DinerMenuIterator.cs
@@ -0,0 +1,38 @@
+﻿using Iterator.InterfaceIterator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Iterator.Menus.ModelforMenus;
+
+namespace Iterator.ConcreteIterator
+{
+    internal class DinerMenuIterator : IIterator
+    {
+            MenuItem[] items;
+            int position = 0;
+
+            public DinerMenuIterator(MenuItem[] items)
+            {
+                this.items = items;
+            }
+            public Object Next()
+            {
+                MenuItem menuItem = items[position];
+                position = position + 1;
+                return menuItem;
+            }
+            public Boolean HasNext()
+            {
+                if(position >=items.Length || items[position] == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+    }
+}
